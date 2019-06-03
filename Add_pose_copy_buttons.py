bc7f26360361a785ed60b5a4282ee53c7c6173fe
leokaze <leokaze@users.noreply.github.com>
2019-06-03 09:44:34 -0400

    Added copy paste flipped button

--- Add_pose_copy_buttons.py
+++ Add_pose_copy_buttons.py
@@ -2,13 +2,23 @@ import bpy
 
 bl_info = {
     "name" : "Add Pose Copy Buttons",
-    "author" : "Syler",
-    "version": (0, 1),
+    "author" : "Syler, leokaze",
+    "version": (0, 2),
     "description": "Adds Pose Copy Buttons To Header",
     "blender" : (2, 80, 0),
     "category" : "3D view"
 }
 
+class CopyPasteFlipPoseOP(bpy.types.Operator):
+    """Copy and paste de flipped current pose for selected bones"""
+    bl_idname = "copypaste.flipped_pose"
+    bl_label = "Copy Paste Flipped Pose"
+    bl_options = {'UNDO', 'REGISTER'}
+
+    def execute(self, context):
+        bpy.ops.pose.copy()
+        bpy.ops.pose.paste(flipped=True)
+        return {'FINISHED'}
 
 
 def menu_func(self, context):
@@ -18,6 +28,7 @@ def menu_func(self, context):
         row.operator("pose.copy", text="", icon='COPYDOWN')
         row.operator("pose.paste", text="", icon='PASTEDOWN').flipped = False
         row.operator("pose.paste", text="", icon='PASTEFLIPDOWN').flipped = True
+        row.operator("copypaste.flipped_pose", text="", icon='ARROW_LEFTRIGHT')
         
 
 classes = [
@@ -25,6 +36,7 @@ classes = [
 
 
 def register():
+    bpy.utils.register_class(CopyPasteFlipPoseOP)
     for c in classes:
         bpy.utils.register_class(c)
 
@@ -35,6 +47,7 @@ def register():
 
 
 def unregister():
+    bpy.utils.unregister_class(CopyPasteFlipPoseOP)
     for c in classes:
         bpy.utils.unregister_class(c)
 
